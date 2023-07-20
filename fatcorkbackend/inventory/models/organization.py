e0b62eacdc78b36013090f1d4133e4f1faa80be3
cameronCJW <cmrnwllc@gmail.com>
2023-07-19 23:05:35 -0700

    format member model

--- fatcorkbackend/inventory/models/organization.py
+++ fatcorkbackend/inventory/models/organization.py
@@ -2,18 +2,23 @@ from django.db import models
 
 from phonenumber_field.modelfields import PhoneNumberField
 
-from base import BaseModel
-from inventory import Cuvee
+from inventory.models.base import BaseModel
+
 
 class Organization(BaseModel):
     pass
 
+
 class Vendor(BaseModel):
     name = models.CharField(max_length=64)
+
 
 class Member(BaseModel):
     name = models.CharField(max_length=64)
     email = models.EmailField()
-    cell_number = PhoneNumberField(null=True, blank=True, unique=True)  # https://django-phonenumber-field.readthedocs.io/en/latest/
-    tried_cuvees = models.ForeignKey(to=Cuvee, on_delete=models.CASCADE)
-
+    # https://django-phonenumber-field.readthedocs.io/en/latest/
+    cell_number = PhoneNumberField(null=True, blank=True, unique=True)
+    tried_cuvees = models.ForeignKey(
+        to='inventory.Cuvee',
+        on_delete=models.CASCADE,
+    )
