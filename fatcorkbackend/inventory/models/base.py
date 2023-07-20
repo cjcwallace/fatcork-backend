e3d5d99c31d918abcdef55366a92d61732b689a6
cameronCJW <cmrnwllc@gmail.com>
2023-07-19 23:05:01 -0700

    modify fields for base model

--- fatcorkbackend/inventory/models/base.py
+++ fatcorkbackend/inventory/models/base.py
@@ -1,12 +1,22 @@
 from django.conf import settings
 from django.db import models
 
-class BaseModel(models.model):
+
+class BaseModel(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     modified_on = models.DateTimeField(auto_now=True)
-    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='%(class)s_createdby')
-    modified_by = models.ForeignKey(settings.AUTH_USER_MODEL,
-                                related_name='%(class)s_modifiedby', null=True, blank=True)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='%(class)s_createdby',
+        on_delete=models.CASCADE,
+    )
+    modified_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='%(class)s_modifiedby',
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+    )
 
     class Meta:
-        abstract=True
+        abstract = True
